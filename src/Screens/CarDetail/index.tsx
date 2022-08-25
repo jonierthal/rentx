3c09d0eb683b8f8e1611783526a2d6e4e86205b0
Jonathan Erthal <jonierthal@gmail.com>
2022-08-25 13:23:55 -0300

    Componente de Slider

--- src/Screens/CarDetail/index.tsx
+++ src/Screens/CarDetail/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
 
 import {
  Container,
- Header
+ Header,
+ <CarImages>
 } from './styles';
 
 export function CarDetail(){
@@ -13,6 +16,12 @@ export function CarDetail(){
             <Header>
                 <BackButton color="black" />
             </Header>
+
+            <CarImages>
+                <ImageSlider 
+                    imagesUrl={['https://img2.gratispng.com/20181115/lej/kisspng-2-18-bmw-m4-coupe-car-2-19-bmw-m4-coupe-bmw-m4-con-pembroke-pines-bmw-i-xdrive-for-sale-in-pembroke-p-5bedce66755684.7137571115423115264806.jpg']}
+                />
+            </CarImages>
        </Container>
     );
 }
