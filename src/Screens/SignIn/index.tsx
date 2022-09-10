b9b2231513435c333fe19a42d6cb415bb6968859
Jonathan Erthal <jonierthal@gmail.com>
2022-09-10 15:33:02 -0300

    Acessibilidade em Formulário

--- src/Screens/SignIn/index.tsx
+++ src/Screens/SignIn/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { 
+    StatusBar,
+    Keyboard,
+    KeyboardAvoidingView, //usado para movimentar os elementos em tela quando o teclado sobe
+    TouchableWithoutFeedback //server para fechar o teclado apertando em qualquer lugar da tela
+} from 'react-native';
 import { useTheme } from 'styled-components';
 
 import { Button } from '../../components/Button'; 
@@ -18,49 +23,53 @@ import {
 export function SignIn(){
     const theme = useTheme();
 
-    return (
-       <Container>
-            <StatusBar 
-                barStyle="dark-content"
-                backgroundColor="transparent"
-                translucent
-            />
-            <Header>
-                <Title>Estamos{'\n'}quase lá.</Title>
-                <SubTitle>
-                    Faça seu login para começar{'\n'}
-                    uma experiência incrível.
-                </SubTitle>
-            </Header>
-            <Form>
-                <Input
-                    iconName="mail"
-                    placeholder="E-mail"
-                    keyboardType="email-address"
-                    auto-correct={false}
-                    autoCapitalize="none"
-                />
-                <PasswordInput
-                    iconName="lock"
-                    placeholder="Senha"
-                />
-            </Form>
-            <Footer>
-                <Button 
-                    title="Login"
-                    onPress={() => {}}
-                    enabled={false}    
-                    loading={false}
-                />
-                <Button 
-                    title="Criar conta gratuita"
-                    onPress={() => {}}
-                    enabled={false}    
-                    loading={false}
-                    color={theme.colors.background_secondary}
-                    light // mesma coisa que passar {true}
-                />
-            </Footer>
-       </Container>
+    return ( //o behavior="position" muda a posição do restante da tela quando o tecado abre
+        <KeyboardAvoidingView behavior="position" enabled> 
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+                <Container>
+                    <StatusBar 
+                        barStyle="dark-content"
+                        backgroundColor="transparent"
+                        translucent
+                    />
+                    <Header>
+                        <Title>Estamos{'\n'}quase lá.</Title>
+                        <SubTitle>
+                            Faça seu login para começar{'\n'}
+                            uma experiência incrível.
+                        </SubTitle>
+                    </Header>
+                    <Form>
+                        <Input
+                            iconName="mail"
+                            placeholder="E-mail"
+                            keyboardType="email-address"
+                            auto-correct={false}
+                            autoCapitalize="none"
+                        />
+                        <PasswordInput
+                            iconName="lock"
+                            placeholder="Senha"
+                        />
+                    </Form>
+                    <Footer>
+                        <Button 
+                            title="Login"
+                            onPress={() => {}}
+                            enabled={false}    
+                            loading={false}
+                        />
+                        <Button 
+                            title="Criar conta gratuita"
+                            onPress={() => {}}
+                            enabled={false}    
+                            loading={false}
+                            color={theme.colors.background_secondary}
+                            light // mesma coisa que passar {true}
+                        />
+                    </Footer>
+            </Container>
+        </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
     );
 }
