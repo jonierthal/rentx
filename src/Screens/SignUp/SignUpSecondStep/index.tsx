3d997fbe841176df144827638f549a2e8d41687b
Jonathan Erthal <jonierthal@gmail.com>
2022-09-14 08:29:09 -0300

    Cadastro de Usuário

--- src/Screens/SignUp/SignUpSecondStep/index.tsx
+++ src/Screens/SignUp/SignUpSecondStep/index.tsx
@@ -11,6 +11,7 @@ import { BackButton } from '../../../components/BackButton';
 import { Bullet } from '../../../components/Bullet';
 import { PasswordInput } from '../../../components/PasswordInput';
 import { Button } from '../../../components/Button';
+import { api } from '../../../services/api';
 
 import {
  Container,
@@ -43,7 +44,7 @@ export function SignUpSecondStep(){
         navigation.goBack();
     }
 
-    function handleRegister() {
+    async function handleRegister() {
         if(!password || !passwordConfirm){
             return Alert.alert('Informa a senha e a confirmação.')
         }
@@ -52,11 +53,22 @@ export function SignUpSecondStep(){
             return Alert.alert('As senhas não são iguais.')
         }
 
-        navigation.navigate('Confirmation', {
-            title: 'Conta criada!',
-            message: `Agora é só fazer login\n e aproveitar`,
-            nextScreenRoute: 'SignIn'
+        await api.post('/users',{
+            name: user.name,
+            email:user.email,
+            driver_license: user.driverLicense,
+            password
         })
+        .then(() => {
+            navigation.navigate('Confirmation', {
+                title: 'Conta criada!',
+                message: `Agora é só fazer login\n e aproveitar.`,
+                nextScreenRoute: 'SignIn'
+            });
+        })
+        .catch(() => {
+            Alert.alert('Opa', 'Não foi possível cadastrar');
+        }); 
     }
 
     return (
