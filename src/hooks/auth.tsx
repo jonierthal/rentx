9c38e7134a591efe8e455d7a91124ecf906eb45e
Jonathan Erthal <jonierthal@gmail.com>
2022-09-21 13:05:27 -0300

    Memory leak

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -48,7 +48,7 @@ function AuthProvider({ children }: AuthProviderProps) {
     
             const { token, user } = response.data;
 
-            api.defaults.headers.authorization = `Bearer ${token}`;
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
             const userCollection = database.get<ModelUser>('users');
             await database.write(async () => {
@@ -76,7 +76,7 @@ function AuthProvider({ children }: AuthProviderProps) {
 
             if(response.length > 0){
                 const userData = response[0]._raw as unknown as User;
-                api.defaults.headers.authorization = `Bearer ${userData.token}`;
+                api.defaults.headers.common['Authorization'] = `Bearer ${userData.token}`;
                 setData(userData);
             }
             
